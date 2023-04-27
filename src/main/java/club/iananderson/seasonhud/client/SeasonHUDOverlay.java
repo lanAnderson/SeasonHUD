00bc02a1d21dcc750a19e30284cf82e78c75704e
Ian Anderson <iananderson92@gmail.com>
2023-04-26 18:56:49 -0700

    ## [1.4.13] - 2023-04-26
    ### Fixed
    
    - Fixed the HUD staying rendered while inventories are open, while using Journeymap or no minimap

--- src/main/java/club/iananderson/seasonhud/client/SeasonHUDOverlay.java
+++ src/main/java/club/iananderson/seasonhud/client/SeasonHUDOverlay.java
@@ -4,6 +4,8 @@ import club.iananderson.seasonhud.config.Location;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.screens.ChatScreen;
+import net.minecraft.client.gui.screens.DeathScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -23,13 +25,13 @@ public class SeasonHUDOverlay {
 
         float guiSize = (float) mc.getWindow().getGuiScale();
 
-        int xOffset = (int) (hudX.get()/guiSize);
-        int yOffset = (int) ((hudY.get())/guiSize);
+        int xOffset = (int) (hudX.get() / guiSize);
+        int yOffset = (int) ((hudY.get()) / guiSize);
         int x = 1;
         int y = 1;
 
         int stringHeight = (mc.font.lineHeight);
-        int iconDim = stringHeight-1;
+        int iconDim = stringHeight - 1;
         int offsetDim = 2;
 
         int stringWidth = mc.font.width(seasonName.get(0)) + iconDim + offsetDim;// might need to take offsetDim out
@@ -51,31 +53,33 @@ public class SeasonHUDOverlay {
                 }
                 case BOTTOM_LEFT -> {
                     x = offsetDim;
-                    y = screenHeight - iconDim - (2*offsetDim);
+                    y = screenHeight - iconDim - (2 * offsetDim);
                 }
                 case BOTTOM_RIGHT -> {
                     x = screenWidth - stringWidth - offsetDim;
-                    y = screenHeight - iconDim - (2*offsetDim);
+                    y = screenHeight - iconDim - (2 * offsetDim);
                 }
             }
 
-            seasonStack.pushPose();
-            seasonStack.scale(1F, 1F, 1F);
+            if ((mc.screen == null || mc.screen instanceof ChatScreen || mc.screen instanceof DeathScreen) && !mc.options.renderDebug) {
+                seasonStack.pushPose();
+                seasonStack.scale(1F, 1F, 1F);
 
-            //Text
-            int iconX = x + xOffset;
-            int iconY = y + yOffset+offsetDim;
-            float textX = (iconX + iconDim + offsetDim);
-            float textY = iconY;//(iconY+(iconDim-stringHeight)); //double check this is exact
-            ForgeGui.getFont().drawShadow(seasonStack, seasonName.get(0),textX, textY, 0xffffffff);
+                //Text
+                int iconX = x + xOffset;
+                int iconY = y + yOffset + offsetDim;
+                float textX = (iconX + iconDim + offsetDim);
+                float textY = iconY;//(iconY+(iconDim-stringHeight)); //double check this is exact
+                ForgeGui.getFont().drawShadow(seasonStack, seasonName.get(0), textX, textY, 0xffffffff);
 
-            //Icon
-            ResourceLocation SEASON = getSeasonResource();
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-            RenderSystem.setShaderTexture(0, SEASON);
-            GuiComponent.blit(seasonStack, iconX, iconY, 0, 0, iconDim, iconDim, iconDim, iconDim);
-            seasonStack.popPose();
+                //Icon
+                ResourceLocation SEASON = getSeasonResource();
+                RenderSystem.setShader(GameRenderer::getPositionTexShader);
+                RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+                RenderSystem.setShaderTexture(0, SEASON);
+                GuiComponent.blit(seasonStack, iconX, iconY, 0, 0, iconDim, iconDim, iconDim, iconDim);
+                seasonStack.popPose();
+            }
         }
     };
 }
