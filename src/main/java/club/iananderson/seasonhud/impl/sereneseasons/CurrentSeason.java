b32da2739aa11125443ec5be2b91da6f54d4290e
Ian Anderson <iananderson92@gmail.com>
2023-05-24 18:30:44 -0700

    ## [1.4.16] - 2023-05-24
    ### Added
    
    - Added support for handling custom sub-season days

--- src/main/java/club/iananderson/seasonhud/impl/sereneseasons/CurrentSeason.java
+++ src/main/java/club/iananderson/seasonhud/impl/sereneseasons/CurrentSeason.java
@@ -5,6 +5,8 @@ import club.iananderson.seasonhud.config.Config;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TranslationTextComponent;
+import sereneseasons.api.config.SeasonsOption;
+import sereneseasons.api.config.SyncedConfig;
 import sereneseasons.api.season.ISeasonState;
 import sereneseasons.api.season.SeasonHelper;
 import java.util.ArrayList;
@@ -57,11 +59,12 @@ public class CurrentSeason {
     public static int getDate() {
         Minecraft mc = Minecraft.getInstance();
         ISeasonState seasonState = SeasonHelper.getSeasonState(Objects.requireNonNull(mc.level));
+        int subSeasonDuration = SyncedConfig.getIntValue(SeasonsOption.SUB_SEASON_DURATION);
 
         int seasonDay = seasonState.getDay(); //total day out of 24 * 4 = 96
 
-        int seasonDate = (seasonDay % 24) + 1; //24 days in a season (8 days * 3 weeks)
-        int subDate = (seasonDay % 8) + 1; //8 days in each subSeason (1 week)
+        int seasonDate = (seasonDay % (subSeasonDuration * 3)) + 1; //24 days in a season (8 days * 3 weeks)
+        int subDate = (seasonDay % subSeasonDuration) + 1; //8 days in each subSeason (1 week)
         int subTropDate = ((seasonDay + 24) % 16) + 1; //16 days in each tropical "subSeason". Starts are "Early Dry" (Summer 1), so need to offset 24 days (Spring 1 -> Summer 1)
 
         if(isTropicalSeason()){
