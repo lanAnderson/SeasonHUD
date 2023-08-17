cf4f5d41508fc5a5f9348251c1cfdecd555cd991
Ian Anderson <iananderson92@gmail.com>
2023-08-16 19:51:08 -0700

    ## [1.5.2] - 2023-08-16
    ### Fixed
    - Fixed Simplified Chinese

--- Common/src/main/java/club/iananderson/seasonhud/data/CurrentLocale.java
+++ Common/src/main/java/club/iananderson/seasonhud/data/CurrentLocale.java
@@ -9,20 +9,13 @@ public class CurrentLocale {
     //Currently implemented languages
     public static String getCurrentLocale(){
         Minecraft mc = Minecraft.getInstance();
-        return mc.getLanguageManager().getSelected().getCode();
+        return mc.getLanguageManager().getSelected().getName().toLowerCase();
     }
     //Improve later, will work for now
     public static List<String> supportedLanguages(){
         List<String> language = new ArrayList<>();
-        language.add("en_au");
-        language.add("en_ca");
-        language.add("en_gb");
-        language.add("en_nz");
-        language.add("en_us");
         language.add("english");
-
-        language.add("zh_cn");
-        language.add("chinese");
+        language.add("简体中文");
         return language;
     }
 
