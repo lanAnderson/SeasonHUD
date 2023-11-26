99848b403cbf6d2c2b5891b203612184e48e2930
IanAnderson <iananderson92@gmail.com>
2023-11-26 11:54:04 -0700

    ## [1.6.2] - 2023-11-26
    ### Fixed
    - Fixed misalignment with latest version of MapAtlases

--- fabric/src/main/java/club/iananderson/seasonhud/client/SeasonHUDClient.java
+++ fabric/src/main/java/club/iananderson/seasonhud/client/SeasonHUDClient.java
@@ -7,8 +7,6 @@ import net.minecraftforge.api.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 
 public class SeasonHUDClient implements ClientModInitializer {
-
-
     @Override
     public void onInitializeClient() {
         ModLoadingContext.registerConfig(SeasonHUD.MOD_ID, ModConfig.Type.CLIENT, Config.GENERAL_SPEC, "SeasonHUD-client.toml");
