e21c2c0ac4c89caf31f77805d29dd2910f1ac52d
IanAnderson <iananderson92@gmail.com>
2024-02-14 09:19:35 -0700

    ## [1.7.12] - 2024-02-14
    ### Fixed
    - Fixed season still displaying when Journeymap is hidden

--- forge/src/main/java/club/iananderson/seasonhud/impl/minimaps/HiddenMinimap.java
+++ forge/src/main/java/club/iananderson/seasonhud/impl/minimaps/HiddenMinimap.java
@@ -1,7 +1,7 @@
 package club.iananderson.seasonhud.impl.minimaps;
 
 import dev.ftb.mods.ftbchunks.client.FTBChunksClientConfig;
-import journeymap.client.JourneymapClient;
+import journeymap.client.ui.UIManager;
 import net.minecraft.client.Minecraft;
 import xaero.common.core.XaeroMinimapCore;
 
@@ -11,7 +11,7 @@ import static club.iananderson.seasonhud.impl.minimaps.CurrentMinimap.loadedMini
 public class HiddenMinimap {
     public static boolean minimapHidden(){
         if (loadedMinimap("journeymap")) {
-            return !JourneymapClient.getInstance().getActiveMiniMapProperties().enabled.get();
+            return !UIManager.INSTANCE.getMiniMap().getCurrentMinimapProperties().enabled.get();
         }
         if (loadedMinimap("ftbchunks")
                 && !loadedMinimap("journeymap")
